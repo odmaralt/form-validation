281c511b56d4bc152da2f957cea46024cf7c6f73
Elbeg Amar <elbeg@nestsolutions.llc>
2022-12-19 17:20:19 -0800

    fixed typescript errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+/* eslint-disable @typescript-eslint/no-non-null-assertion */
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root")!);
 root.render(
   <React.StrictMode>
     <App />
